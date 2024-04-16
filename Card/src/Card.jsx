f3e5c0a3d51781bc76075c49d93ca74c79547687
Shivanand Kumar <132545822+shivanand91@users.noreply.github.com>
2024-04-16 19:00:59 +0530

    Add button in card component

--- Card/src/Card.jsx
+++ Card/src/Card.jsx
@@ -7,6 +7,7 @@ function Card(){
             <img src={profilePic} alt="profile picture" />
             <h2>Shivanand Kumar</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum deleniti ipsum unde non maiores aliquid nostrum quos incidunt magni iure nihil distinctio dolorum maxime, atque error doloribus veniam, recusandae reprehenderit.</p>
+             <a href="#" className='button'> Read More</a>
         </div>
         </>
     );
