a49fd6d71d51a84b9f8587050a4c22f4f5d6e164
Shivanand Kumar <132545822+shivanand91@users.noreply.github.com>
2024-04-17 17:23:36 +0530

    External CSS and Inline CSS

--- Card/src/Card.jsx
+++ Card/src/Card.jsx
@@ -1,5 +1,17 @@
 import profilePic from './assets/profile1.jpg'
 function Card(){
+
+    //  Inline CSS
+    const style = {
+        
+        // all the attribution must use in camelCase
+        textDecoration: "none",
+        color: "white",
+        padding: "5px",
+        borderRadius: "10px",
+        border: "2px solid white",
+        backgroundColor: "rgb(23, 10, 169)",
+    }
   
     return(
         <>
@@ -7,7 +19,9 @@ function Card(){
             <img src={profilePic} alt="profile picture" />
             <h2>Shivanand Kumar</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum deleniti ipsum unde non maiores aliquid nostrum quos incidunt magni iure nihil distinctio dolorum maxime, atque error doloribus veniam, recusandae reprehenderit.</p>
-             <a href="#" className='button'> Read More</a>
+             {/* this comment line is use for external css */}
+             {/* <a href="#" className='button'> Read More</a> */}
+             <a href="#" style={style}> Read More</a>
         </div>
         </>
     );
