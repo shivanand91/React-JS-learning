0296e0b29a75994e0805b8d344588f06c840921c
Shivanand Kumar <132545822+shivanand91@users.noreply.github.com>
2024-04-18 23:05:44 +0530

    UserGreeting by using props and default props

--- Usergretting/src/App.jsx
+++ Usergretting/src/App.jsx
@@ -3,7 +3,9 @@ function App() {
 
   return (
     <>
-     <UserGreeting />
+     <UserGreeting  isLoggedIn={false} username="Shivanand" />
+     <UserGreeting  isLoggedIn={true} username="Shivanand" />
+     <UserGreeting  isLoggedIn={true} />
     </>
   )
 }
--- Usergretting/src/UserGreeting.jsx
+++ Usergretting/src/UserGreeting.jsx
@@ -1,11 +1,24 @@
+import PropTypes from 'prop-types'
+function UserGreeting(props){
 
-function UserGreeting(){
-    return(
+        
+         if(props.isLoggedIn){
+          return <h2 className="loggedIn">Welcome, {props.username}</h2> 
+         } 
+         else{
+             return <h2 className="notLoggedIn">Please log in to continue</h2>
+         }
+        
+}
 
-        <>
-        <h1>Hello, My name is shivanand kumar</h1>
-        </>
-    );
+UserGreeting.PropTypes = {
+    isLoggedIn: PropTypes.bool,
+    username: PropTypes.string,
+}
+
+UserGreeting.defaultProps = {
+    isLoggedIn: false,
+    username: 'Guest'
 }
 
 export default UserGreeting
